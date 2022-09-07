ea9ab6c24d79640951c64bbb189ca9346ce2bf0c
Yatharth-Tomar <yatharth_tomar@srmap.edu.in>
2022-09-07 19:22:42 +0530

    initial commit

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const connect = async () => {
   try {
     const check = await mongoose.connect(
-      'mongodb://0.0.0.0:27017/recordsofEmployers'
+      'mongodb+srv://Yatharth:123@cluster0.b7nygtc.mongodb.net/?retryWrites=true&w=majorityrecordsofEmployers'
     );
     console.log('Connection to database successfull :)');
   } catch (e) {
